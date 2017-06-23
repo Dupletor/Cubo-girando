63fe9e0223ef1ed2fd21f36fd3577214e2cf6d42
Dupletor <fabio.aguiar@usp.br>
2017-06-23 12:27:42 -0300

    Commit
    Isto eh um commit.

---
+++ Assets/OVR/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Time.deltaTime*100, -100*Time.deltaTime, 50*Time.deltaTime);
+	}
+}
