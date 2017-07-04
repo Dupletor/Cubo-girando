085dadee4ceea1b73c4cf68d40a38a85259df357
Dupletor <fabio.aguiar@usp.br>
2017-07-04 15:37:59 -0300

    Implementando controle
    Por botoes, o cubo para de girar

--- Assets/OVR/Scripts/Halt.cs
+++ Assets/OVR/Scripts/Halt.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Halt : MonoBehaviour {
 
-public class Spin : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		global.canSpin = true;	
@@ -11,11 +11,9 @@ public class Spin : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(global.canSpin)
-			transform.Rotate(Time.deltaTime*100, -100*Time.deltaTime, 50*Time.deltaTime);
+		if (Input.GetMouseButtonDown (0) && Physics.Raycast (transform.position, transform.forward, 500))
+			global.canSpin = false;
+		else
+			global.canSpin = true;
 	}
-}
-
-public static class global {
-	public static bool canSpin;
 }
